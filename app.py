40bfb288f1c4a82e30beb39ff8d5193076deadd7
Miraç ZORLU <miraczorlu2@gmail.com>
2025-03-10 18:09:14 +0300

    Docker and Github action changes commit

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def predict():
         final_input = scaler.transform(np.array(data).reshape(1,-1))
         print(final_input)
         output=regmodel.predict(final_input)[0]
-        return render_template("home.html ", prediction_text="The House price prediction is {}".format(output))
+        return render_template("home.html ", prediction_text="Ev fiyatı tahmini    {:.3f}  bin dolar ".format(output*-1))
         
 
 if __name__=="__main__":
