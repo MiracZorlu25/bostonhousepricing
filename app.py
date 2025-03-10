178f4b5cb8f18f87ded939e7b6de46e4e5631668
Miraç ZORLU <miraczorlu2@gmail.com>
2025-03-10 12:34:22 +0300

    the web app is ready

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
+import json
 import pickle
-from flask import Flask, request, jsonify, jsonify,url_for, render_template
-from django.shortcuts import render 
+from flask import Flask,request,app,jsonify,url_for,render_template
 import numpy as np
 import pandas as pd
-
 
 
 app = Flask(__name__)
@@ -17,8 +16,7 @@ def home():
     return render_template('home.html')
 
 @app.route('/predict_api', methods=['POST'])
-def predict_api():
-    # Get the data from the POST request.
+def predict_api():  # Get the data from the POST request.
     data = request.json['data']
     print(data)
     print(np.array(list(data.values())).reshape(1,-1))
@@ -33,8 +31,10 @@ def predict():
         final_input = scaler.transform(np.array(data).reshape(1,-1))
         print(final_input)
         output=regmodel.predict(final_input)[0]
-        return render_template('home.html',prediction_text="The House price prediction is {}".format(output))
+        return render_template("home.html ", prediction_text="The House price prediction is {}".format(output))
         
 
-if __name__ == '__main__':
+if __name__=="__main__":
     app.run(debug=True)
+   
+     
